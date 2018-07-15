372a9faac4b61c70c50621daea386a1db815b406
Rio Astamal <me@rioastamal.net>
2018-07-15 12:19:46 +0800

    Chapter 7: Closure function

---
+++ chapter7/function-closure.go
@@ -0,0 +1,32 @@
+package main
+import "fmt"
+
+func main() {
+    var tmpInput int
+    var index uint8 = 1
+
+    // Create closure to determine a given number is odd or even
+    oddEven := func(number int) string {
+        // Closure can access "global" variable where its defined
+        // Here we update value of index
+        index++
+
+        if number % 2 == 0 {
+            return "even"
+        }
+        return "odd"
+    }
+
+    // Put infinite loop
+    fmt.Println("Determine number is odd or even.\nType -1 to exit program.")
+    fmt.Println("----------------")
+    for true {
+        fmt.Printf("Try #%d - Input a number: ", index)
+        fmt.Scanf("%d", &tmpInput)
+        fmt.Printf("Number %v is %v.\n", tmpInput, oddEven(tmpInput))
+
+        if tmpInput == -1 { break }
+    }
+
+    fmt.Println("\nSee you later.")
+}
