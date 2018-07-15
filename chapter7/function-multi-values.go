9189322248cd3bbe5b11478f4a38d701eb8e4e00
Rio Astamal <me@rioastamal.net>
2018-07-15 08:16:44 +0800

    Chapter 7: Add another return value for the function

--- chapter7/function-multi-values.go
+++ chapter7/function-multi-values.go
@@ -3,7 +3,6 @@ import "fmt"
 
 func main() {
     scores := make([]float64, 0)
-    var total float64
     var index uint8 = 0
     var tmp_input float64
 
@@ -19,14 +18,12 @@ func main() {
 
         // Resize score slice
         scores = append(scores, tmp_input)
-        total += tmp_input
         index++
     }
 
+    avg_result, total, odd_even, avg_result_int := avg(scores)
     fmt.Println("-------------")
     fmt.Printf("We have %d score(s) with total %v.\n", index, total)
-
-    avg_result, odd_even, avg_result_int := avg(scores)
     fmt.Printf("Average score is %v.\n", avg_result)
     fmt.Printf("Average score is %v number (integer -> %v).\n", odd_even, avg_result_int)
 }
@@ -40,10 +37,11 @@ func main() {
  *
  * @param []float64 numbers
  * @return float64 average score
+ * @return float64 sum score
  * @return string odd or even number
  * @return int convert the avg to int
  */
-func avg(numbers []float64) (float64, string, int) {
+func avg(numbers []float64) (float64, float64, string, int) {
     var total float64
     var result float64
 
@@ -60,12 +58,13 @@ func avg(numbers []float64) (float64, string, int) {
     // Return values
     // 1. Average score. Need to convert length of the numbers so it has
     //    same data type float64
-    // 2. Even or odd number
-    // 3. Integer version of the result
+    // 2. Sum of the score
+    // 3. Even or odd number
+    // 4. Integer version of the result
     result = total / float64( len(numbers) )
 
     if int(result) % 2 == 0 {
-        return result, "even", int(result)
+        return result, total, "even", int(result)
     }
-    return result, "odd", int(result)
+    return result, total, "odd", int(result)
 }
